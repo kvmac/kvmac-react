bdadb2a4baf97d22ddcdd213efe05ff702c83125
Kodee McIntosh <kodee.mcintosh@gmail.com>
2019-05-23 12:15:49 -0400

    fix attempt 7

--- src/lambdas/mailgun.js
+++ src/lambdas/mailgun.js
@@ -16,25 +16,24 @@ export async function handler(event, context, callback) {
     console.log('------------- event:  ', event, '       typeof:  ', typeof event);
     // const event = JSON.parse(event);
     let data = JSON.parse(event.body);
-
-    // if(!data.from
-    //   || !data.subject
-    //   || !data.text) {
-    //     console.log('body:  ', data, '  type:  ', typeof(data));
-    //     console.log('MADE INTO NULL CHECK', 'from: ',data.from, '... subject: ', data.subject, '... text: ', data.text);
-    //   return;
-    // }
-    data.to = { 'to': 'kodee.mcintosh@kvmac.com' };
-    data = JSON.stringify(data);
-
     console.log('NEW CALL ----------------------------------------');
     console.log('body:  ', data, '  type:  ', typeof(data));
     console.log('MADE INTO HANDLER', 'from: ', data.from, '... subject: ', data.subject, '... text: ', data.text);
 
+    if(!data.from
+      || !data.subject
+      || !data.text) {
+        console.log('body:  ', data, '  type:  ', typeof(data));
+        console.log('MADE INTO NULL CHECK', 'from: ',data.from, '... subject: ', data.subject, '... text: ', data.text);
+      return;
+    }
+    data.to = 'kodee.mcintosh@kvmac.com';
+    data = JSON.stringify(data);
+
+
     let res = await mailgun.messages().send(domain, data)
-    .then((res) => console.log(res)) // logs response data
-    .catch((err) => console.warn(err)); // logs any error
-    console.log('response:  ', res);
+    .then((res) => console.log('response: ', res)) // logs response data
+    .catch((err) => console.warn('Error in the mailgun client:  ', err)); // logs any error
 
     if (!res.ok) {
       return {
