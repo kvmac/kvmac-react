370aeb68b81a33bc73c526d8e6b6774b9988c453
Kodee McIntosh <kodee.mcintosh@gmail.com>
2019-05-24 14:15:13 -0400

    did some blog sub route stuff

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,39 +3,29 @@ import '../styles/blog.css';
 import { Link, Route, Switch } from 'react-router-dom';
 import BlogPosts from './blog-posts';
 
-export class Blog extends React.Component {
-  constructor(props) {
-    super(props);
+export const Blog = ({ match }) => {
 
-    this.state = {
+  return (
+    <div className="blog">
+      <div className="card">
+      <div className="links">
+        {BlogPosts.map((post, i) => {
+          return (
+            <li key={i}>
+              <Link to={`/blog/:${post.key}`}>{post.key}</Link>
+            </li>
+          );
+        })}
+      </div>
 
-    };
-  }
-
-  render() {
-
-    return (
-      <div className="blog">
-        <div className="card">
-        <div className="links">
+      <div className="post">
           {BlogPosts.map((post, i) => {
             return (
-              <li key={i}>
-                <Link to={`/blog/:${post.key}`}>{post.key}</Link>
-              </li>
+              <Route path={`${match.url}/:${post.key}`} component={post.value} />
             );
           })}
-        </div>
-
-        <div className="post">
-            {BlogPosts.map((post, i) => {
-              return (
-                <Route path={`/blog/:${post.key}`} component={post.value} />
-              );
-            })}
-        </div>
-        </div>
       </div>
-    );
-  }
+      </div>
+    </div>
+  );
 }
